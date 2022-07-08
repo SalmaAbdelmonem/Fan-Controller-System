d2276a1f77fc057d586f856ec3bd98c04837f6c1
SalmaAbdelmonem <96205385+SalmaAbdelmonem@users.noreply.github.com>
2022-07-08 17:20:54 +0200

    Add files via upload

---
+++ Mini Project 3/Mini_Project3/Debug/Mini_Project3.d
@@ -0,0 +1,14 @@
+Mini_Project3.o Mini_Project3.o: ../Mini_Project3.c ../adc.h \
+  ../std_types.h ../lcd.h ../lm35_sensor.h ../pwm.h ../dc_motor.h
+
+../adc.h:
+
+../std_types.h:
+
+../lcd.h:
+
+../lm35_sensor.h:
+
+../pwm.h:
+
+../dc_motor.h:
